895d4fe4a0e6e47d6daef2e51067eb29dc4bcc8c
deter0 <deter0109@gmail.com>
2023-02-10 21:07:07 -0500

    Restructuring

--- src/tile_renderer.cpp
+++ src/tile_renderer.cpp
@@ -143,6 +143,148 @@ p_fn err_code tr_tile_map_cam_input(TileMap *map, ALLEGRO_KEYBOARD_STATE *state,
   return ERR_OKAY;
 }
 
+p_private p_fn void tr_draw_grid(TileMap *map) {
+  for (uint x = 0; x < map->map_renderer_res.x; x += map->atlas.texture_size) {
+    al_draw_line(
+      x, 0,
+      x, map->map_renderer_res.y,
+      al_map_rgba_f(1.0f, 0, 1.0f, 0.45f),
+      1.5f
+    );
+  }
+  for (uint y = 0; y < map->map_renderer_res.y; y += map->atlas.texture_size) {
+    al_draw_line(
+      0, y,
+      map->map_renderer_res.x, y,
+      al_map_rgba_f(1.f, 0.f, 1.f, 0.45f),
+      1.5f
+    );
+  }
+}
+
+#define SEL_PADDING (10.f)
+p_private p_fn void tr_draw_selection_ui(TileMap *map, float screen_width, float screen_height, float mouse_x, float mouse_y) {
+  float selection_window_size_x = 280, selection_window_size_y = screen_height - SEL_PADDING*2.f;
+  
+  ALLEGRO_BITMAP *screen_bitmap = al_get_target_bitmap();
+  ALLEGRO_BITMAP *selection_menu_framebuffer;
+  if (rm_get_image("selection_menu_render", &selection_menu_framebuffer) == ERR_RM_NOT_FOUND || selection_menu_framebuffer == NULL) {
+    selection_menu_framebuffer = al_create_bitmap(selection_window_size_x, selection_window_size_y);
+    rm_insert_image("selection_menu_render", selection_menu_framebuffer);
+  }
+  assert(selection_menu_framebuffer != NULL);
+  
+  ALLEGRO_BITMAP *items_framebuffer;
+  if (rm_get_image("selection_menu_items_render", &items_framebuffer) == ERR_RM_NOT_FOUND || items_framebuffer == NULL) {
+    items_framebuffer = al_create_bitmap(selection_window_size_x - SEL_PADDING*2, selection_window_size_y-SEL_PADDING*3-22);
+    rm_insert_image("selection_menu_items_render", items_framebuffer);
+  }
+  assert(items_framebuffer != NULL);
+  
+  al_set_target_bitmap(selection_menu_framebuffer);
+  glm::vec2 selection_menu_size = glm::vec2(al_get_bitmap_width(selection_menu_framebuffer),
+                                            al_get_bitmap_height(selection_menu_framebuffer));
+  glm::vec2 items_framebuffer_size = glm::vec2(al_get_bitmap_width(items_framebuffer),
+                                                al_get_bitmap_height(items_framebuffer));
+  
+  al_draw_filled_rectangle(0, 0, selection_menu_size.x, selection_menu_size.y, al_map_rgb(60, 60, 60));
+  al_draw_rectangle(0, 0, selection_menu_size.x, selection_menu_size.y, al_map_rgb(125, 125, 125), 2.f);
+
+  ALLEGRO_FONT *mw_20;
+  p_ASSERT_ERR(rm_get_font("mw_20", &mw_20));
+  
+  al_draw_text(mw_20, al_map_rgb(255, 255, 255),
+              SEL_PADDING, SEL_PADDING,
+              0, "Select Image");
+  
+  al_set_target_bitmap(items_framebuffer);
+  al_clear_to_color(al_map_rgba(0, 0, 0, 0));
+  float texture_scale = 3.f;
+  float texture_display_size = map->atlas.texture_size * texture_scale;
+  float texture_y = 0, texture_x = 0;
+
+  al_hold_bitmap_drawing(true);
+  uint total = 0;
+  for (uint n = 0; n < map->atlas.total; n++) {
+    u32 tex_coord_x = (n % map->atlas.row_count) * map->atlas.texture_size;
+    u32 tex_coord_y = (n / map->atlas.row_count) * map->atlas.texture_size;
+    
+    float texture_render_pos_x = texture_x;
+    float texture_render_pos_y = texture_y + GLOBAL_START_SELECTION;
+    
+    if (texture_render_pos_y + texture_display_size < 0) {
+      texture_x += texture_display_size + SEL_PADDING*2;
+      if ((texture_x + texture_display_size) > items_framebuffer_size.x) {
+        texture_x = 0;
+        texture_y += texture_display_size + SEL_PADDING*2;
+      }
+      continue;
+    }
+    
+    al_draw_filled_rectangle(texture_render_pos_x,
+                              texture_render_pos_y,
+                              texture_render_pos_x + texture_display_size,
+                              texture_render_pos_y + texture_display_size,
+                              al_map_rgba(90, 90, 90, 165));
+    float border_offset = 2.f;
+    
+    ALLEGRO_COLOR outline_color = al_map_rgb(155, 155, 155);
+
+    float relative_mouse_x = (mouse_x - SEL_PADDING*2);
+    float relative_mouse_y = (mouse_y - (screen_height - selection_menu_size.y) - SEL_PADDING*2-22);
+    al_draw_circle(relative_mouse_x, relative_mouse_y, 4, outline_color, 5.f);
+    if (relative_mouse_x >= texture_render_pos_x && relative_mouse_x <= texture_render_pos_x+texture_display_size
+          && relative_mouse_y >= texture_render_pos_y && relative_mouse_y <= texture_render_pos_y+texture_display_size) {
+      outline_color = al_map_rgb(255, 0, 0);
+      
+      ALLEGRO_MOUSE_STATE mouse_state;
+      al_get_mouse_state(&mouse_state);
+      
+      if (mouse_state.buttons & 1) {
+        currently_drawing = n;
+      }
+    } else if (currently_drawing == n) {
+      outline_color = al_map_rgb(155, 0, 0);
+    }
+    
+    al_draw_rectangle(texture_render_pos_x - border_offset,
+                      texture_render_pos_y - border_offset,
+                      texture_render_pos_x + texture_display_size + border_offset,
+                      texture_render_pos_y + texture_display_size + border_offset,
+                      outline_color, 2.0f);
+
+    al_draw_tinted_scaled_rotated_bitmap_region(map->atlas.atlas_image,
+                                                tex_coord_x, tex_coord_y, // Source x,y
+                                                map->atlas.texture_size, map->atlas.texture_size, // Source w,h
+                                                al_map_rgb_f(1.f, 1.f, 1.f), // Tint
+                                                0.f, 0.f, // Center x,y
+                                                texture_render_pos_x, texture_render_pos_y, // Dest x,y
+                                                texture_scale, texture_scale, // Dest w,h
+                                                0.f, 0);
+    texture_x += texture_display_size + SEL_PADDING*2;
+    if ((texture_x + texture_display_size) > items_framebuffer_size.x) {
+      texture_x = 0;
+      texture_y += texture_display_size + SEL_PADDING*2;
+    }
+    
+    total++;
+    if (texture_y > items_framebuffer_size.y - GLOBAL_START_SELECTION) {
+      break;
+    }
+  }
+
+  al_hold_bitmap_drawing(false);
+  al_set_target_bitmap(selection_menu_framebuffer);
+  
+  al_draw_bitmap_region(items_framebuffer, 0, 0, items_framebuffer_size.x, items_framebuffer_size.y,
+                        SEL_PADDING, SEL_PADDING*3+22, 0);
+
+  al_set_target_bitmap(screen_bitmap);
+  float selection_top_left_x = SEL_PADDING,
+        selection_top_left_y = screen_height - selection_menu_size.y - SEL_PADDING;
+  al_draw_bitmap(selection_menu_framebuffer, selection_top_left_x, selection_top_left_y, 0);
+}
+
 p_fn err_code tr_tile_map_render(TileMap *map, float mouse_x, float mouse_y, ALLEGRO_KEYBOARD_STATE *state, bool debug) {
   u32 texture_size = map->atlas.texture_size;
   
@@ -173,27 +315,10 @@ p_fn err_code tr_tile_map_render(TileMap *map, float mouse_x, float mouse_y, ALL
   al_hold_bitmap_drawing(true);
   
   if (debug) {
-    for (uint x = 0; x < map->map_renderer_res.x; x += texture_size) {
-      al_draw_line(
-        x, 0,
-        x, map->map_renderer_res.y,
-        al_map_rgba_f(1.0f, 0, 1.0f, 0.45f),
-        1.5f
-      );
-    }
-    for (uint y = 0; y < map->map_renderer_res.y; y += texture_size) {
-      al_draw_line(
-        0, y,
-        map->map_renderer_res.x, y,
-        al_map_rgba_f(1.f, 0.f, 1.f, 0.45f),
-        1.5f
-      );
-    }
+    tr_draw_grid(map);
   }
 
   if (debug) {
-    al_draw_rectangle(selection_x, selection_y, selection_x+texture_size, selection_y+texture_size, al_map_rgba(0, 255, 0, 155), 2.5f);
-    
     if (map->data.tiles != NULL) {
       u32 index = (selection_y/texture_size) * map->tiles_count_x + (selection_x/texture_size);
       if (al_key_down(state, ALLEGRO_KEY_F)) {
@@ -232,6 +357,14 @@ p_fn err_code tr_tile_map_render(TileMap *map, float mouse_x, float mouse_y, ALL
   }
 
   al_hold_bitmap_drawing(false);
+  
+  if (debug) {
+    al_draw_rectangle(selection_x, selection_y,
+                      selection_x+texture_size,
+                      selection_y+texture_size,
+                      al_map_rgba(0, 255, 0, 155), 2.5f);
+  }
+  
   al_set_target_bitmap(previous_target);
   
   al_draw_scaled_bitmap(map->map_renderer_bitmap, 0, 0,
@@ -252,129 +385,10 @@ p_fn err_code tr_tile_map_render(TileMap *map, float mouse_x, float mouse_y, ALL
     rm_get_font("mw_20", &mw_20);
     al_draw_textf(mw_20, al_map_rgba(255, 0, 255, 200), 12, 12, 0, "Selection: %d, %d", (int)selection_x/texture_size, (int)selection_y/texture_size);
     
-    // Sprite Selection Menu
-    {
-      uint screen_width = al_get_bitmap_width(previous_target);
-      uint screen_height = al_get_bitmap_height(previous_target);
-      
-      float selection_window_size_x = screen_width - 12, selection_window_size_y = 250;
-      float SEL_PADDING = 10.f;
-      
-      ALLEGRO_BITMAP *selection_menu_framebuffer;
-      if (rm_get_image("selection_menu_render", &selection_menu_framebuffer) == ERR_RM_NOT_FOUND || selection_menu_framebuffer == NULL) {
-        selection_menu_framebuffer = al_create_bitmap(screen_width - SEL_PADDING * 2, selection_window_size_y);
-        rm_insert_image("selection_menu_render", selection_menu_framebuffer);
-      }
-      assert(selection_menu_framebuffer != NULL);
-      
-      ALLEGRO_BITMAP *items_framebuffer;
-      if (rm_get_image("selection_menu_items_render", &items_framebuffer) == ERR_RM_NOT_FOUND || items_framebuffer == NULL) {
-        items_framebuffer = al_create_bitmap(selection_window_size_x - SEL_PADDING*2, selection_window_size_y-SEL_PADDING*3-22);
-        rm_insert_image("selection_menu_items_render", items_framebuffer);
-      }
-      assert(items_framebuffer != NULL);
-      
-      al_set_target_bitmap(selection_menu_framebuffer);
-      glm::vec2 selection_menu_size = glm::vec2(al_get_bitmap_width(selection_menu_framebuffer),
-                                                al_get_bitmap_height(selection_menu_framebuffer));
-      glm::vec2 items_framebuffer_size = glm::vec2(al_get_bitmap_width(items_framebuffer),
-                                                   al_get_bitmap_height(items_framebuffer));
-      
-      al_draw_filled_rectangle(0, 0, selection_menu_size.x, selection_menu_size.y, al_map_rgb(60, 60, 60));
-      al_draw_rectangle(0, 0, selection_menu_size.x, selection_menu_size.y, al_map_rgb(125, 125, 125), 2.f);
-
-      al_draw_text(mw_20, al_map_rgb(255, 255, 255),
-                  SEL_PADDING, SEL_PADDING,
-                  0, "Select Image");
-      
-      al_set_target_bitmap(items_framebuffer);
-      al_clear_to_color(al_map_rgba(0, 0, 0, 0));
-      float texture_scale = 0.2f;
-      float texture_display_size = map->atlas.texture_size * texture_scale;
-      float texture_y = 0, texture_x = 0;
-
-      al_hold_bitmap_drawing(true);
-      uint total = 0;
-      for (uint n = 0; n < map->atlas.total; n++) {
-        u32 tex_coord_x = (n % map->atlas.row_count) * map->atlas.texture_size;
-        u32 tex_coord_y = (n / map->atlas.row_count) * map->atlas.texture_size;
-        
-        float texture_render_pos_x = texture_x;
-        float texture_render_pos_y = texture_y + GLOBAL_START_SELECTION;
-        
-        if (texture_render_pos_y + texture_display_size < 0) {
-          texture_x += texture_display_size + SEL_PADDING*2;
-          if ((texture_x + texture_display_size) > items_framebuffer_size.x) {
-            texture_x = 0;
-            texture_y += texture_display_size + SEL_PADDING*2;
-          }
-          continue;
-        }
-        
-        al_draw_filled_rectangle(texture_render_pos_x,
-                                 texture_render_pos_y,
-                                 texture_render_pos_x + texture_display_size,
-                                 texture_render_pos_y + texture_display_size,
-                                 al_map_rgba(90, 90, 90, 165));
-        float border_offset = 2.f;
-        
-        ALLEGRO_COLOR outline_color = al_map_rgb(155, 155, 155);
-
-        float relative_mouse_x = (mouse_x - SEL_PADDING*2);
-        float relative_mouse_y = (mouse_y - (screen_height - selection_menu_size.y) - SEL_PADDING*2-22);
-        al_draw_circle(relative_mouse_x, relative_mouse_y, 4, outline_color, 5.f);
-        if (relative_mouse_x >= texture_render_pos_x && relative_mouse_x <= texture_render_pos_x+texture_display_size
-              && relative_mouse_y >= texture_render_pos_y && relative_mouse_y <= texture_render_pos_y+texture_display_size) {
-          outline_color = al_map_rgb(255, 0, 0);
-          
-          ALLEGRO_MOUSE_STATE mouse_state;
-          al_get_mouse_state(&mouse_state);
-          
-          if (mouse_state.buttons & 1) {
-            currently_drawing = n;
-          }
-        } else if (currently_drawing == n) {
-          outline_color = al_map_rgb(155, 0, 0);
-        }
-        
-        al_draw_rectangle(texture_render_pos_x - border_offset,
-                          texture_render_pos_y - border_offset,
-                          texture_render_pos_x + texture_display_size + border_offset,
-                          texture_render_pos_y + texture_display_size + border_offset,
-                          outline_color, 1.0f);
-
-        al_draw_tinted_scaled_rotated_bitmap_region(map->atlas.atlas_image,
-                                                    tex_coord_x, tex_coord_y, // Source x,y
-                                                    map->atlas.texture_size, map->atlas.texture_size, // Source w,h
-                                                    al_map_rgb_f(1.f, 1.f, 1.f), // Tint
-                                                    0.f, 0.f, // Center x,y
-                                                    texture_render_pos_x, texture_render_pos_y, // Dest x,y
-                                                    texture_scale, texture_scale, // Dest w,h
-                                                    0.f, 0);
-        texture_x += texture_display_size + SEL_PADDING*2;
-        if ((texture_x + texture_display_size) > items_framebuffer_size.x) {
-          texture_x = 0;
-          texture_y += texture_display_size + SEL_PADDING*2;
-        }
-        
-        total++;
-        if (texture_y > items_framebuffer_size.y - GLOBAL_START_SELECTION) {
-          break;
-        }
-      }
-      printf("Rendered: %d\n", total);
-      al_hold_bitmap_drawing(false);
-      al_set_target_bitmap(selection_menu_framebuffer);
-      
-      al_draw_bitmap_region(items_framebuffer, 0, 0, items_framebuffer_size.x, items_framebuffer_size.y,
-                            SEL_PADDING, SEL_PADDING*3+22, 0);
-                  
-      al_set_target_bitmap(previous_target);
-      
-      float selection_top_left_x = SEL_PADDING,
-            selection_top_left_y = screen_height - selection_menu_size.y - SEL_PADDING;
-      al_draw_bitmap(selection_menu_framebuffer, selection_top_left_x, selection_top_left_y, 0);
-    }
+    al_set_target_bitmap(previous_target); 
+    tr_draw_selection_ui(map, (float)al_get_bitmap_width(previous_target),
+                        (float)al_get_bitmap_height(previous_target),
+                        mouse_x, mouse_y);
   }
   
   return ERR_OKAY;
